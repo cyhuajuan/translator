bd147f0916bd9663574f7e9aa6ac1bdff2d7f7b8
cyhuajuan <cyhuajuan@outlook.com>
2026-04-12 00:33:09 +0800

    chore: configure vite and npm scripts for tauri integration

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,35 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
+const host = process.env.TAURI_DEV_HOST;
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  clearScreen: false,
+  server: {
+    // make sure this port matches the devUrl port in tauri.conf.json file
+    port: 5173,
+    // Tauri expects a fixed port, fail if that port is not available
+    strictPort: true,
+    // if the host Tauri is expecting is set, use it
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+
+    watch: {
+      // tell vite to ignore watching `src-tauri`
+      ignored: ['**/src-tauri/**'],
     },
   },
 })
